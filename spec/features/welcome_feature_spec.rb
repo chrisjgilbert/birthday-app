ed23a5774687b3f74143310187a50cc0b3e7fad7
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-15 17:10:39 +0000

    index setup

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -1,6 +1,11 @@
 feature 'the home page' do
   scenario 'welcomes the user' do
     visit '/'
-    expect(page).to have_content 'welcome'
+    expect(page).to have_content 'Welcome!'
+  end
+
+  scenario 'asks the user for their name' do
+    visit '/'
+    expect(page).to have_content 'What is your name?'
   end
 end
